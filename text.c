ff64d6493d1237191bfe46d681e8ea676a882bf7
23133ZK <115622123+23133ZK@users.noreply.github.com>
2023-03-03 21:21:01 +0800

    Add files via upload

--- text.c
+++ text.c
@@ -4,19 +4,15 @@
 #include<string.h>
 int main()
 {
-	int s = 0, v = 0,t=0,j=0,tem=0;
-	char h[2] = { 0 }, m[2] = { 0 };
-	scanf("%d%d", &s, &v);
-	t = s / v;
-	if (s % v != 0)
-		tem = 469;
+	int k = 0;
+	double Pi = 3.14,v=0,h = 0, r = 0, j = 0;
+	scanf("%lf%lf", &h, &r);
+	v = Pi * r * r * h;
+	j = 20000 / v;
+	k = j;
+	if(j==k)
+	printf("%.0f\n", j);
 	else
-		tem = 470;
-	j =tem - t;
-	h[0] = j / 60 / 10;
-	h[1] = j / 60 % 10;
-	m[0] = j % 60 / 10;
-	m[1] = j % 60 % 10;
-	printf("%d%d:%d%d", h[0], h[1], m[0], m[1]);
+		printf("%.0f\n", j+1);
 	return 0;
 }
